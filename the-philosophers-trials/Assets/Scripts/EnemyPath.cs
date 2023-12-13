9c9767ec472fb3aadf05b5035c8af28b7a641056
bryantd0208 <72286876+bryantd0208@users.noreply.github.com>
2023-12-13 18:11:00 -0500

    Monster and Movement

--- the-philosophers-trials/Assets/Scripts/EnemyPath.cs
+++ the-philosophers-trials/Assets/Scripts/EnemyPath.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement; 
 
 public class EnemyPath : MonoBehaviour
 {
@@ -11,20 +12,21 @@ public class EnemyPath : MonoBehaviour
     [SerializeField] private Movement playerMovement; // Reference to the player's movement script
     [SerializeField] private float baseDetectionRange = 6f;
     [SerializeField] private float pathUpdateThreshold = 0.1f; // Define a threshold for path update
-
+    [SerializeField] private AudioClip pathfindingSound;
+    private AudioSource audioSource;
     private float detectionRange;
     private int waypointIndex = 0;
     private Pathfinding pathfinding;
-    private const float AttackRange = 0.5f; // Define the range for attacking
+    private const float AttackRange = 1f; // Define the range for attacking
     private bool followingPath;
 
     void Start()
     {
         transform.position = waypoints[waypointIndex].transform.position;
         pathfinding = new Pathfinding(gridVisualizer); // Create a new instance of Pathfinding
+        audioSource = GetComponent<AudioSource>();
         StartCoroutine(PrintPlayerState());
     }
-
     void Update()
     {
         UpdateDetectionRange();
@@ -34,7 +36,7 @@ public class EnemyPath : MonoBehaviour
             Vector3 playerPosition = playerTransform.position;
             if (!followingPath || Vector3.Distance(transform.position, playerPosition) > pathUpdateThreshold)
             {
-                StopAllCoroutines(); // Stop the current path-following coroutine
+                StopAllCoroutines();
                 FindPathToPlayer(playerPosition);
             }
         }
@@ -43,7 +45,13 @@ public class EnemyPath : MonoBehaviour
             MoveToWaypoints();
         }
     }
-
+    private void PlayPathfindingSound()
+    {
+        if (audioSource != null)
+        {
+            audioSource.Play(); // Play the audio clip
+        }
+    }
     private void UpdateDetectionRange()
     {
         if (playerMovement.IsRunning)
@@ -74,7 +82,9 @@ public class EnemyPath : MonoBehaviour
     {
         if (transform.position != waypoints[waypointIndex].transform.position)
         {
-            transform.position = Vector3.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
+            Vector3 nextPosition = waypoints[waypointIndex].transform.position;
+            OrientTowards(nextPosition); // Update orientation
+            transform.position = Vector3.MoveTowards(transform.position, nextPosition, moveSpeed * Time.deltaTime);
         }
         else
         {
@@ -82,48 +92,57 @@ public class EnemyPath : MonoBehaviour
         }
     }
 
-    private void FindPathToPlayer(Vector3 playerPosition)
+ private void FindPathToPlayer(Vector3 playerPosition)
+{
+    pathfinding.FindPath(transform.position, playerPosition);
+    if (gridVisualizer.path != null && gridVisualizer.path.Count > 0)
     {
-        pathfinding.FindPath(transform.position, playerPosition);
-        if (gridVisualizer.path != null && gridVisualizer.path.Count > 0)
+        if (!followingPath) // Check if the enemy is not already following a path
         {
-            StopAllCoroutines();
-            StartCoroutine(FollowPath(gridVisualizer.path));
+            // PlayPathfindingSound(); // Play sound when starting to pathfind
+            followingPath = true; // Set followingPath to true here
         }
+        StopAllCoroutines();
+        StartCoroutine(FollowPath(gridVisualizer.path));
     }
+}
 
 IEnumerator FollowPath(System.Collections.Generic.List<Node> path)
+{
+    foreach (var node in path)
     {
-        followingPath = true;
-
-        for (int i = 0; i < path.Count - 1; i++)
+        Vector3 nextPosition = node.worldPosition;
+        while (Vector3.Distance(transform.position, nextPosition) > 0.1f)
         {
-            Node node = path[i];
-            // Move towards each node in the path except the last one
-            while (Vector3.Distance(transform.position, node.worldPosition) > 0.1f)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, node.worldPosition, moveSpeed * Time.deltaTime);
-                yield return null;
+            OrientTowards(nextPosition); 
+            transform.position = Vector3.MoveTowards(transform.position, nextPosition, moveSpeed * Time.deltaTime);
+
+            if(Vector3.Distance(transform.position, playerTransform.position) <= AttackRange) {
+                AttackPlayer();
+                yield break;
             }
-        }
-
-        // Handle the last node (player's position) differently
-        Node lastNode = path[path.Count - 1];
-        while (Vector3.Distance(transform.position, lastNode.worldPosition) > AttackRange)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, lastNode.worldPosition, moveSpeed * Time.deltaTime);
             yield return null;
         }
-
-        // Trigger attack
-        AttackPlayer();
-
-        followingPath = false;
     }
 
-    private void AttackPlayer()
+    followingPath = false; // Reset followingPath to false here
+}
+
+
+    // New method to orient the enemy towards the target position
+    private void OrientTowards(Vector3 targetPosition)
+    {
+        Vector3 direction = (targetPosition - transform.position).normalized;
+        if (direction != Vector3.zero)
+        {
+            Quaternion lookRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * moveSpeed);
+        }
+    }
+
+private void AttackPlayer()
     {
         Debug.Log("Attacked player");
-        // Here you can add more logic for what happens when the player is attacked.
+        SceneManager.LoadSceneAsync(0);
     }
 }
